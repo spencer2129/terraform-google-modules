1181a1909b37677bf864c1f63c9c7ee6d85ddb73
Spencer Aranda <spencer.aranda90@gmail.com>
2025-09-19 16:57:33 -0600

    Add terraform configuration with proper gitignore

---
+++ net-ext-lb/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "nad-anthos-dev-cloud"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
